a1a3b9b3de593059c7361bd1dd88ac17ef5d5922
lightrojan <33711527+lightrojan@users.noreply.github.com>
2020-03-01 23:20:12 +0800

    Changed robustness of code

--- windows_automation_disable_enable.py
+++ windows_automation_disable_enable.py
@@ -2,12 +2,12 @@
 """
 Created on Sun Mar  1 17:00:33 2020
 
-@author: zhong
+@author: zhong cheng
 
 Disable/Enable Test for 2.4G/5G
 """
 
-import os 
+import os
 import ctypes, sys
 from time import sleep
 import time
@@ -15,12 +15,13 @@ from urllib.request import urlopen
 
 true_count = 0
 false_count = 0
+runs = 11
 
 def internet_on(): #check for internet connectivity
     try:
         response = urlopen('https://www.google.com/', timeout=10)
         return True
-    except: 
+    except:
         return False
 
 def enable():
@@ -28,10 +29,10 @@ def enable():
 
 def disable():
     os.system('netsh interface set interface "Wi-Fi" disabled')
-    
+
 def status():
-    os.system('netsh interface show interface | findstr /C:”Wi-Fi” /C:”Name”')
-    
+    os.system('netsh interface show interface | findstr /C:"Wi-Fi" /C:"Name"')
+
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
@@ -40,8 +41,9 @@ def is_admin():
 
 if is_admin():
     assert internet_on() == True, 'Not connected' #Error checking
-    
-    for i in range(0,10):
+
+    for i in range(1,runs):
+        print('=======Running Test Number '+str(i)+'=======')
         print("Disabling card...")
         disable()
         status()
@@ -49,6 +51,8 @@ if is_admin():
         if internet_on() == False:
             print("Enabling card...")
             enable()
+            print("Connect to wifi...")
+            sleep(5)
             status()
             sleep(5)
             if internet_on() == True:
@@ -59,8 +63,12 @@ if is_admin():
         print('True Count:' + str(true_count))
         print('False Count:' + str(false_count))
         sleep(5)
+    print('Test finished Running!')
+    if true_count == runs:
+        print('Test passed!')
+    else:
+        print('Test Failed!')
 
 else:
     # Re-run the program with admin rights
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-    exit()
